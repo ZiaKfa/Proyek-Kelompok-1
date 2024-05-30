25be89deb6ed4e8c7bd88169301f82b0011bf6d9
Bintang <itzbintangyosua@gmail.com>
2024-05-30 18:23:57 +0700

    menambahkan movies.ts

---
+++ movies.ts
@@ -0,0 +1,29 @@
+type Movie = {
+  name: string;
+  year: number;
+  genre: string;
+  description: string;
+};
+
+const movies: Movie[] = [
+  {
+    name: "The Prestige",
+    year: 2006,
+    genre: "Drama",
+    description: "Some kind of magic stuff",
+  },
+  {
+    name: "Interstellar",
+    year: 2014,
+    genre: "Sci-Fi",
+    description:
+      "When Earth becomes uninhabitable in the future, a farmer and ex-NASA pilot, Joseph Cooper, is tasked to pilot a spacecraft",
+  },
+  {
+    name: "Batman: The Dark Knight",
+    year: 2008,
+    genre: "Action",
+    description:
+      "Batman must accept one of the greatest psychological tests of his ability to fight injustice",
+  },
+];
